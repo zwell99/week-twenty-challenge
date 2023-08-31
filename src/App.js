84ad680e20d92fc21fbc3a212b36d9b24ee556ff
Zach Wellens <zachwellens990@gmail.com>
2023-08-31 17:04:43 -0400

    Added About Me Component

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import Header from './components/Header';
 import Project from './components/Project';
 import Footer from './components/Footer';
 import ContactForm from './components/ContactForm.js';
+import AboutMe from './components/AboutMe';
 
 function App() {
-  // Makes the default state be the protfolio
-  const [page, setPage] = useState('Portfolio');
+  // Makes the default state be the About Me page
+  const [page, setPage] = useState('About me');
 
   return (
     <div className="bucket-app">
       <Header page={page} setPage={setPage} />
-      {(page == 'About me') ? (<p> ipsum lorem </p>): 
+      {(page == 'About me') ? (<AboutMe />): 
       (page == 'Portfolio') ? (<ul>
-        <li><Project img={imgtemp} title={temptitle} deployedLink={templink} githubLink={templink} /></li>
-        <li><Project img={imgtemp} title={temptitle} deployedLink={templink} githubLink={templink} /></li>
+        <li><Project img={imgtemp} title={temptitle} deployedLink={templink} githubLink={'https://github.com/stevenarroyave8/interactive-front-end-application'} /></li>
+        <li><Project img={imgtemp} title={temptitle} deployedLink={templink} githubLink={'https://github.com/andreaber23/Budgify'} /></li>
         <li><Project img={imgtemp} title={temptitle} deployedLink={templink} githubLink={templink} /></li>
       </ul>):
-      (page == 'Contact me') ? (<ContactForm />):
-      (<p> ipsum lorem </p>)}
+      (page == 'Contact') ? (<ContactForm />):
+      (<a src='https://drive.google.com/file/d/1P_AgGf_ZKVFAAivjC_0nXa2k-8BmMT84/view?usp=sharing'>Resume Link</a>)}
       <Footer />
     </div>
   );
